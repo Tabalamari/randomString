4b78c282835c9db8355a8514ef31d3e8cbeb8114
tabalamari <tabalamari@gmail.com>
2021-08-14 20:09:00 +0300

    it works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,47 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'It is MY NEW TITLE';
-  res = r;
-  getColor(){
-      return fontColor;
-  }
-  getDisplay(){
-      return stringDisplay;
-  }
+export class AppComponent implements OnInit {
+
+    res: any
+
+    title = 'It is MY NEW TITLE';
+    getColor() {
+        return fontColor;
+    }
+    getDisplay() {
+        return stringDisplay;
+    }
+
+    ngOnInit() {
+        interval(3000).subscribe(() => {
+            this.res = ''
+            stringDisplay = "initial";
+            for (let i = 0; i < 5; i++) {
+                const randomIndex = Math.floor(Math.random() * alphabet.length);
+                this.res += alphabet[randomIndex]
+            }
+            if (this.res[0] == this.res[4] && this.res[1] == this.res[3]) {
+                fontColor = "red";
+            }
+            if (!isNaN(Number(this.res))) {
+                fontColor = "blue";
+            }
+            if (this.res.indexOf("0") >= 0) {
+                stringDisplay = "none";
+            }
+        })
+    }
+
 }
-let r = '';
+
 const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890";
-for (let i = 0; i < 5; i++) {
-    const randomIndex = Math.floor(Math.random() * alphabet.length);
-    r += alphabet[randomIndex]
-}
 let fontColor = "black";
-if (r[0] == r[4] && r[1] == r[3]) {
-    fontColor = "red";
-}
-if (!isNaN(Number(r))) {
-    fontColor = "blue";
-}
 let stringDisplay = "initial";
-if (r.indexOf("0") >= 0){
-    stringDisplay = "none";
-}
