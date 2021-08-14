7a37149b1d9cb4bce02d5bdce74de54a9059fc02
tabalamari <tabalamari@gmail.com>
2021-08-14 14:28:31 +0300

    conditions are done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'It is MY NEW TITLE';
   res = r;
+  getColor(){
+      return fontColor;
+  }
+  getDisplay(){
+      return stringDisplay;
+  }
 }
 let r = '';
-const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890";
 for (let i = 0; i < 5; i++) {
     const randomIndex = Math.floor(Math.random() * alphabet.length);
     r += alphabet[randomIndex]
+}
+let fontColor = "black";
+if (r[0] == r[4] && r[1] == r[3]) {
+    fontColor = "red";
+}
+if (!isNaN(Number(r))) {
+    fontColor = "blue";
+}
+let stringDisplay = "initial";
+if (r.indexOf("0") >= 0){
+    stringDisplay = "none";
 }
